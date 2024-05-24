b0983f22faf6b2dea755aa91819b4412ebcdeefe
Dyldevlab <153741222+Dyldevlab@users.noreply.github.com>
2024-05-24 16:15:40 +0200

    nice

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home({ searchParams }: BlogPageProps) {
 
   return (
     <>
-      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-8 lg:py-32">
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-6 lg:py-32">
         <div className="container flex flex-col gap-4 text-center">
           <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
             Bienvenue sur mon blog
